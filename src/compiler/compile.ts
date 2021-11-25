842e56781d5dc58f2398c1b2369c845a95b74c4a
stagas <gstagas@gmail.com>
2021-11-25 21:10:50 +0200

    refactor: decouple the generator from the builder

--- src/compiler/compile.ts
+++ src/compiler/compile.ts
@@ -1,29 +1,37 @@
 import { ParserNode, panic } from '../parser'
 import type { SExpr } from './sexpr'
 
-const generator = <T>(build: (arg: T) => SExpr) => {
-  const instr = (name: string, ...nodes: T[]) => [name, ...nodes.map(build)]
-
+const generator = () => {
   const ifelse = (
     resultType: string,
-    condition: T,
-    ifBody: T,
-    elseBody?: T
+    condition: SExpr,
+    ifBody: SExpr,
+    elseBody?: SExpr
   ) => {
     return [
       'if',
       ['result', resultType],
-      ['i32.trunc_f32_s', build(condition)],
-      ['then', build(ifBody)],
-      elseBody ? ['else', build(elseBody)] : null,
+      f32.cast_to_boolean(condition),
+      ['then', ifBody],
+      elseBody ? ['else', elseBody] : null,
     ]
   }
 
-  return { instr, ifelse }
+  // prettier-ignore
+  const f32 = {
+    cast_to_boolean: (value: SExpr) => ['i32.trunc_f32_s', value],
+    negate_arithmetic: (value: SExpr) => ['f32.mul', ['f32.const', '-1'], value],
+    logical_not: (value: SExpr) => [
+      'f32.convert_i32_u',
+      ['i32.eqz', f32.cast_to_boolean(value)],
+    ]
+  }
+
+  return { ifelse, f32 }
 }
 
 export const build = (node: ParserNode): SExpr => {
-  const { instr, ifelse } = generator<ParserNode>(build)
+  const { ifelse, f32 } = generator()
 
   if (!Array.isArray(node))
     switch (node.group) {
@@ -59,7 +67,7 @@ export const build = (node: ParserNode): SExpr => {
       case 'ops':
         switch (symbol.value) {
           case '?':
-            return ifelse('f32', lhs, mhs, rhs)
+            return ifelse('f32', build(lhs), build(mhs), build(rhs))
         }
       default:
         throw new SyntaxError(panic('ternary op not implemented', symbol))
@@ -70,13 +78,13 @@ export const build = (node: ParserNode): SExpr => {
       case 'ops':
         switch (symbol.value) {
           case '-':
-            return instr('f32.sub', lhs, rhs)
+            return ['f32.sub', build(lhs), build(rhs)]
           case '+':
-            return instr('f32.add', lhs, rhs)
+            return ['f32.add', build(lhs), build(rhs)]
           case '*':
-            return instr('f32.mul', lhs, rhs)
+            return ['f32.mul', build(lhs), build(rhs)]
           case '/':
-            return instr('f32.div', lhs, rhs)
+            return ['f32.div', build(lhs), build(rhs)]
         }
       default:
         throw new SyntaxError(panic('binary op not implemented', symbol))
